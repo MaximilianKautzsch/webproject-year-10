f39b4973bcfdf4d5692ac6240c3b8418419eaec1
Maximilian Kautzsch <maximilian.kautzsch@icloud.com>
2024-04-17 19:46:40 +0200

    Comment questions.js

--- src/js/questions.js
+++ src/js/questions.js
@@ -1,4 +1,9 @@
-// creating an array and passing the number, questions, options, and answers
+/**
+ * @file questions.js
+ * @brief JavaScript file containing an array of quiz questions.
+ */
+
+// Array containing quiz questions, options, and correct answers
 let questions = [
   {
     numb: 1,
@@ -35,17 +40,4 @@ let questions = [
     answer: "Glauchau",
     options: ["Zwickau", "Glauchau", "Limbach-Oberfrohna", "Werdau"],
   },
-  // you can uncomment the below codes and make duplicate as more as you want to add question
-  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
-  //   {
-  //   numb: 6,
-  //   question: "Your Question is Here",
-  //   answer: "Correct answer of the question is here",
-  //   options: [
-  //     "Option 1",
-  //     "option 2",
-  //     "option 3",
-  //     "option 4"
-  //   ]
-  // },
 ];
